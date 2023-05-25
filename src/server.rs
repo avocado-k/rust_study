e2043f6cd5f75d770d32c5473a5dd2c8f442e5b2
Avocado <march31po@gmail.com>
2023-05-26 00:02:44 +0900

    :construction: add methods using traits

--- src/server.rs
+++ src/server.rs
@@ -1,28 +1,42 @@
 use std::net::TcpListener;
-
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::io::Read;
+use std::convert::Tryinto;
 
 pub struct Server {
     addr: String,
-
 }
+
 
 impl Server {
     pub fn new(addr: String) -> Self {
         Self { addr }
     }
 
-    pub fn run(self){
+    pub fn run(self) {
         println!("listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-        loop{
+        loop {
             match listener.accept() {
-                OK((stream, _)) =>{
-                    let a = 5;
-                    println!("ok");
-                },
-                Err(e) => println!("failed to establish a connection {}",e),
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024]; //0 for 1024 bites
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+
+                        }
+                        Err(e) => println!("failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("failed to establish a connection {}", e),
                 //_ => println!("failed to establish a connection"),//default
             }
         }
